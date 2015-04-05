2d31a1b9d8037810760aaebde8cd8a48b158da07
Ozzie Gooen <Ozzie@BowLabs.org>
2015-04-05 10:17:22 -0700

    Update frequency_list.py

--- frequency_list.py
+++ frequency_list.py
@@ -6,6 +6,20 @@
 ##First, put this file in the same directory as a csv file with one row of relevant data.  
 ##Name that data data.csv
 ##In the console, cd into that folder and run 'python numplot.py'
+##Instructions:
+##First, put this file in the same directory as a csv file with one row of relevant data.  
+##Name that data data.csv
+##In the console, cd into that folder and run 'python numplot.py'
+
+##Instructions:
+##First, put this file in the same directory as a csv file with one row of relevant data.  
+##Name that data data.csv
+##In the console, cd into that folder and run 'python numplot.py'
+
+##Instructions:
+##First, put this file in the same directory as a csv file with one row of relevant data.  
+##Name that data data.csv
+##In the console, cd into that folder and run 'python numplot.py'
 
 ##Example Input: A CSV file with 1 row 82 columns, each cell containing a 1,2 or 3
 ##Example Output: 
