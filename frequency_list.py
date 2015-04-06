4ff23a272cc62402db1cd1ed44240158eaa8b11d
Ozzie Gooen <Ozzie@BowLabs.org>
2015-04-06 00:54:22 -0700

    test birds

--- frequency_list.py
+++ frequency_list.py
@@ -62,3 +62,34 @@ def main(script, *args):
 if __name__ == '__main__':
     import sys
     main(*sys.argv)
+    
+subspecies and the sexes. The wings and lateral tail feathers are bluish 
+while the tail is dark green. The legs are grey. The female is 
+similar to the male though duller in colouration and has an underwing 
+stripe, which is not present in the adult male. Juveniles are duller
+than females and have an underwing stripe.[7]
+Distribution and habitat[edit]
+Natural range is eastern Australia, down to Tasmania. The eastern
+rosella is found in lightly wooded country, open forests, 
+woodlands, gardens, bushlands and parks.
+The eastern rosella (Platycercus eximius) has
+become naturalised in New Zealand.[8] By the 1970s
+the population, probably originally from cage escapees,
+strongly established throughout Auckland, Northland, & 
+the far north, extending into west Waikato, as far south
+as Kawhia, & Te Kuiti, & East to the Coromandel Peninsula.
+Also in the Wellington-Hutt Valley Region, established in the 
+1960s from escaped cage birds, later colonising the foothills 
+of the Tararua Range, to Eketahuna in the east, & Otaki in the
+west[9] (range up to 1985). Sightings from New Plymouth, Taupo,
+Gisborne, Tiritea, Banks Peninsula, Nelson area, & Stewart Island.
+The first occurrence of these parrots in New Zealand was about 
+1910 when a small shipment of eastern rosellas, as well as a few 
+crimson rosellas (P. elegans), that had been refused entry into
+New Zealand by the Customs Department was released off Otago Heads
+by the ship that brought them, as she was returning to Sydney. 
+The two species crossed [& by 1955] no pure Crimson Rosellas 
+remained in the Dunedin area.[10] The population of rosellas in 
+Dunedin has always remained low, partially due to them being 
+trapped & sold as caged birds, & that the climate can be extremely 
+cold in comparison to their native homeland.
